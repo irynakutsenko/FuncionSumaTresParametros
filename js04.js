a9d2f2e1707293dbf15ec99d28d2c067f28dd3aa
Iryna Kutsenko <79517296+irynakutsenko@users.noreply.github.com>
2021-10-14 09:21:10 +0200

    FuncionSumaTresParametros

---
+++ js04.js
@@ -0,0 +1,12 @@
+let num1 = parseInt(prompt("Añade primer numero"));
+
+let num2 = parseInt(prompt("Añade otro numero mas"));
+suma(num1, num2);
+
+
+function suma(num1, num2) {
+    let result = num1 + num2;
+    let total = result;
+
+    console.log(total);
+}
